edc254f5df2b1c6a2fccf93595ba5036ed862dcb
Afra <nurafraciftci@gmail.com>
2023-07-15 17:21:42 +0200

    v5

--- src/components/ContainerCard.jsx
+++ src/components/ContainerCard.jsx
@@ -28,7 +28,7 @@ const ContainerCard = () => {
         onChange={handleChange}
       />
       <Container className="p-3 rounded-4 card-container my-3">
-        <Row xs={1} sm={2} md={3} lg={4} className="justify-content-center g-3">
+        <Row xs={1} sm={2} md={3} lg={4} className="justify-content-center g-4">
     
           {filteredData.map((player, i) => (
             <PlayerCard key={i} {...player} />
